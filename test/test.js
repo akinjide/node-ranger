deae26e6f261e4087fe78c0ddd9f1fcdd30e0e45
Akinjide Bankole <r@akinjide.me>
2018-05-14 10:33:43 +0100

    test: add test for asynchronous methods

--- test/test.js
+++ test/test.js
@@ -11,77 +11,99 @@ var ranger = require('..');
  * Tests
  */
 describe('ranger()', function() {
-  describe('#getArray()', function() {
-    it('should return an Array of length 5', function() {
-      var range = ranger.getArray(1, 10, 2);
 
-      assert.equal(range.length, 5);
-      assert.equal(typeof range, typeof []);
-      assert.equal(range[1], [3]);
+  describe('#fill()', function() {
+    it('Promise: should return an Array of length 4', function(done) {
+      var range = ranger.fill(0.2, 4.0);
+      range.should.be.Promise();
+      range.should.be.fulfilledWith([0, 1, 2, 3]);
+      range.should.eventually.have.length(4);
+      done();
     });
 
-    it('should return an array of length 10 without step', function() {
-      var range = ranger.getArray(1, 10);
-
-      assert.equal(range.length, 10);
-      assert.equal(typeof range, typeof []);
-      assert.equal(range[5], [6]);
+    it('Callback: should return an Array of length 4', function(done) {
+      ranger.fill(0, 4, 1, function(error, result) {
+        result.should.be.an.Array().and.containDeep([0, 1, 2, 3]);
+      });
+      done();
     });
 
-    it('should return an error if start and stop are missing', function() {
-      var range = ranger.getArray();
+    it('Promise: should throw an error if no start argument', function(done) {
+      ranger.fill().catch(function(error) {
+      	error.message.should.be.eql('fill() expected least 1 argument');
+      });
+      done();
+    });
 
-      assert.equal(range, "start and stop are required!");
+    it('Promise: should throw an error if step argument is 0', function(done) {
+      var range = ranger.fill(1, 10, 0);
+      range.should.be.Promise();
+      range.catch(function(error) {
+      	error.message.should.be.eql('fill() step must not be zero');
+      });
+      done();
+    });
+
+    it('Callback: should throw an error if step argument is 0', function(done) {
+      ranger.fill(1, 10, 0, function(error, result) {
+      	error.message.should.be.eql('fill() step must not be zero');
+      });
+      done();
+    });
+
+    it('Promise: should return an Empty Array if start is greater than stop', function(done) {
+      var range = ranger.fill(4, 1, 2);
+      range.should.be.Promise();
+      range.should.be.fulfilledWith([]);
+      range.should.eventually.have.length(0);
+      done();
+    });
+
+    it('Callback: should return an Empty Array if start is greater than stop', function(done) {
+      ranger.fill(4, 1, 2, function(err, result) {
+        result.should.be.an.Array().and.containDeep([]);
+      });
+      done();
+    });
+
+    it('Promise: should return an Array of length 5 if step is a negative number', function(done) {
+      var range = ranger.fill(10, 1, -2);
+      range.should.be.Promise();
+      range.should.be.fulfilledWith([10, 8, 6, 4, 2]);
+      range.should.eventually.have.length(5);
+      done();
+    });
+  });
+
+
+  describe('#object()', function() {
+    it('Promise: should return an Object of length 5', function(done) {
+      var range = ranger.object(2, 40, 9);
+      range.should.be.Promise();
+      range.should.be.fulfilledWith({ '0': 2, '1': 11, '2': 20, '3': 29, '4': 38 });
+      done();
+    });
+
+    it('Promise: should return an Object of length 4', function(done) {
+      var range = ranger.object(1, 10, 2);
+      range.should.be.Promise();
+      range.should.be.fulfilledWith({ '0': 1, '1': 3, '2': 5, '3': 7, '4': 9 });
+      done();
+    });
+
+    it('Promise: should return an Object of length 5 without step', function(done) {
+      var range = ranger.object(1, 5);
+      range.should.be.Promise();
+      range.should.be.fulfilledWith({ '0': 1, '1': 2, '2': 3, '3': 4 });
+      done();
+    });
+
+    it('Promise: should return an error if start and stop are missing', function(done) {
+      ranger.object().catch(function(error) {
+        error.message.should.be.eql('object() expected least 1 argument')
+      });
+      done();
     });
   });
 
-
-  describe('#getObject()', function() {
-    it('should return an Object', function() {
-      var range = ranger.getObject(1, 10, 2);
-
-      assert.equal(typeof range, typeof {});
-      assert.equal(range['0'], 1);
-      assert.equal(range['1'], 3);
-      assert.equal(typeof range['1'], typeof 3);
-    });
-
-    it('should return an Object of length 5 without step', function() {
-      var range = ranger.getObject(1, 5);
-
-      assert.equal(Object.keys(range).length, 5);
-      assert.equal(typeof range, typeof {});
-    });
-
-    it('should return an error if start and stop are missing', function() {
-      var range = ranger.getObject();
-
-      assert.equal(range, "start and stop are required!");
-    });
-  });
-
-
-  describe('#fill() regular for..loop', function() {
-    it('should return an Array of length 5', function() {
-      var range = ranger.fill(1, 10, 2);
-
-      assert.equal(range.length, 5);
-      assert.equal(typeof range, typeof []);
-      assert.equal(range[1], [3]);
-    });
-
-    it('should return an array of length 10 without step', function() {
-      var range = ranger.fill(1, 10);
-
-      assert.equal(range.length, 10);
-      assert.equal(typeof range, typeof []);
-      assert.equal(range[5], [6]);
-    });
-
-    it('should return an error if start and stop are missing', function() {
-      var range = ranger.fill();
-
-      assert.equal(range, "start and stop are required!");
-    });
-  });
 });
